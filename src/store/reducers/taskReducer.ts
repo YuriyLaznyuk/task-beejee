ce3a3cf4ac407e61f7c0252ad68d2c103fc8ff83
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-29 13:27:04 +0200

    feat: added TaskList

--- src/store/reducers/taskReducer.ts
+++ src/store/reducers/taskReducer.ts
@@ -10,10 +10,12 @@ const initialState: TaskState = {
 export const taskReducer = (state = initialState, action: TaskAction) => {
 	switch (action.type) {
 		case TaskActionType.FETCH_TASK:
+			const pages = Math.ceil(action.payload.totalTask / 3);
 			return {
 				...state,
 				tasks: action.payload.tasks,
 				totalTask: action.payload.totalTask,
+				totalPage: pages,
 			};
 		default:
 			return state;
